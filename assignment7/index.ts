8415362f7b2e32e8bb3fbe8938180142e6f8edc0
Hanan Khalid <hanankhalid1434@gmail.com>
2023-09-22 23:11:04 +0500

    assignment7 of calculator and inquirer files added

---
+++ assignment7/index.ts
@@ -0,0 +1,70 @@
+import inquirer, { Answers, Question, QuestionCollection } from "inquirer";
+
+let questions: QuestionCollection = [
+  {
+    type: "number",
+    name: "num1",
+    message: "Enter Number 1 : ",
+  },
+  {
+    type: "number",
+    name: "num2",
+    message: "Enter Number 2 : ",
+  },
+  {
+    type: "list",
+    name: "operator",
+    message: "Select Operator:",
+    choices: ["+", "-", "*", "/", "%", "^"],
+  },
+];
+
+let answers: Promise<Answers> = inquirer.prompt(questions);
+answers
+  .then((answer) => {
+    switch (answer.operator) {
+      case "+":
+        console.log(
+          `${answer.num1} + ${answer.num2} = ${answer.num1 + answer.num2}`
+        );
+        break;
+      case "-":
+        console.log(
+          `${answer.num1} - ${answer.num2} = ${answer.num1 - answer.num2}`
+        );
+        break;
+
+      case "*":
+        console.log(
+          `${answer.num1} * ${answer.num2} = ${answer.num1 * answer.num2}`
+        );
+        break;
+
+      case "/":
+        console.log(
+          `${answer.num1} + ${answer.num2} = ${answer.num1 / answer.num2}`
+        );
+        break;
+      case "%":
+        console.log(
+          `${answer.num1} % ${answer.num2} = ${answer.num1 % answer.num2}`
+        );
+        break;
+      case "+":
+        console.log(
+          `${answer.num1} + ${answer.num2} = ${answer.num1 + answer.num2}`
+        );
+        break;
+      case "^":
+        console.log(
+          `${answer.num1} ^ ${answer.num2} = ${answer.num1 ** answer.num2}`
+        );
+        break;
+
+      default:
+        break;
+    }
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
---
+++ assignment7/q1.ts
@@ -0,0 +1,75 @@
+// import inquirer, { Answers } from "inquirer";
+import chalk from "chalk";
+
+
+// async function calculateGrade() {
+//     let result = await inquirer.prompt([
+//         {
+//             type: "number",
+//             name: "marks",
+//             message: "Enter your Marks: "
+//         }
+//     ]);
+
+//     if (result.marks >= 0 && result.marks < 50) {
+//         console.log("Your Grade is D");
+//     }
+//     else if (result.marks >= 50 && result.marks < 60) {
+//         console.log("Your grade is C");
+//     }
+//     else if (result.marks >= 60 && result.marks < 70) {
+//         console.log("Your grades are B");
+//     }
+//     else if (result.marks >= 70 && result.marks < 80) {
+//         console.log("You got A");
+//     }
+//     else if (result.marks >= 80 && result.marks <= 100) {
+//         console.log("You got A+");
+//     }
+//     else {
+//         console.log('Invalid input');
+//     }
+// }
+
+// calculateGrade();
+
+// async function gradeSystem() {
+//     try {
+//         const answer: Answers = await inquirer.prompt([
+//             {
+//                 type: "number",
+//                 name: "marks",
+//                 message: "Enter your Marks: "
+//             }
+//         ]);
+
+//         if (answer.marks > 100 || answer.marks < 0) {
+//             throw  Error("Enter Correct Marks");
+//         } else if (answer.marks >= 0 && answer.marks < 50) {
+//             console.log("Your Grade is D");
+//         } else if (answer.marks >= 50 && answer.marks < 60) {
+//             console.log("Your grade is C");
+//         } else if (answer.marks >= 60 && answer.marks < 70) {
+//             console.log("Your grades are B");
+//         } else if (answer.marks >= 70 && answer.marks < 80) {
+//             console.log("You got A");
+//         } else if (answer.marks >= 80 && answer.marks <= 100) {
+//             console.log("You got A+");
+//         }
+//     } 
+//     catch(error)  {
+//         console.log("Error is produced!");
+//     }
+//     finally{
+//         console.log("Finally");
+        
+//     }
+// }
+
+// gradeSystem();
+
+
+
+
+
+
