30f3f428b80952fd921130db6f13e9f35b3e8501
Hanan Khalid <hanankhalid1434@gmail.com>
2023-10-16 12:33:35 +0500

    file name changinng

---
+++ assignment8(ATM)/q1.js
@@ -0,0 +1,5 @@
+let myType = { name: "Zia", id: 1 };
+let myType1 = { id: 2, name: "Tom", age: 22 };
+myType = myType1;
+console.log(myType);
+export {};
---
+++ assignment8(ATM)/q1.ts
@@ -0,0 +1,4 @@
+let myType = { name: "Zia", id:  1}
+let myType1 = { id: 2, name: "Tom", age: 22 }; 
+myType = myType1;
+console.log(myType)
